b84bdfe3afc9bff885367b3d4c475b9b8c9d62da
khaidar_ar <96arrijal@gmail.com>
2022-07-25 18:47:50 +0700

    config animate on-scroll

--- script.js
+++ script.js
@@ -48,18 +48,17 @@ function serviceHovers() {
         }
     }
 }
+const numbs = document.querySelectorAll('.skill-title p:nth-of-type(2)');
+const times = document.querySelectorAll('.skill-content .skill-inner .skill-fill-in');
 
-function prog() {
-    const times = document.querySelectorAll('.skill-content .skill-inner .skill-fill-in');
-    times.forEach(time => {
-        time.addEventListener('webkitAnimationStart', precentageAnimationBar());
+function prog(pNumbs, pTimes) {
+    pTimes.forEach(time => {
+        time.addEventListener('webkitAnimationStart', precentageAnimationBar(pNumbs));
     })
 }
 
-function precentageAnimationBar() {
-    const numbs = document.querySelectorAll('.skill-title p:nth-of-type(2)');
-
-    numbs.forEach(numb => {
+function precentageAnimationBar(pNumbs) {
+    pNumbs.forEach(numb => {
         let numbDisplay = numb.textContent;
         let iterator = 0;
         let time = 0;
@@ -73,11 +72,12 @@ function precentageAnimationBar() {
         // ========== iterate-display-number-precentage ==========
         const iterateNumb = () => {
             if (iterator < numbDisplay) {
-                numb.textContent = iterator++ + '%';
+                numb.textContent = iterator++;
                 setTimeout(iterateNumb, time - 10);
             } else {
-                numb.textContent = numbDisplay + '%';
+                numb.textContent = numbDisplay;
             }
+
         }
         iterateNumb();
     });
@@ -92,7 +92,6 @@ function typeAnimation() {
     const text_Content = ['Khaidar', 'Web developer', 'Software Engineer', 'Tech Enthusiast'];
     let temp = '';
     let showText = '';
-
     if (cursor === text_Content.length) {
         cursor = 0;
     }
@@ -103,15 +102,42 @@ function typeAnimation() {
         cursor++;
         pointer = 0;
     }
-    setTimeout(typeAnimation, 400);
+    setTimeout(typeAnimation, 200);
 }
 
 function invokeWhenScroll() {
+    // const box = document.querySelector('.text-content');
+    const bd = document.querySelector('body');
+    const getPages = document.querySelectorAll('.onScroll');
+    const bar = document.querySelector('.about');
+    window.addEventListener('scroll', () => {
+        const onScreen = window.innerHeight;
+        let breakpoint = 840;
+        getPages.forEach(page => {
+            let onViews = page.getBoundingClientRect().top;
+            // onViews -= 0.01;
+
+            if (onViews < onScreen / 10) {
+                page.classList.add('active');
+                if (bar) {
+                    prog(numbs, times)
+                }
+            } else {
+                page.classList.remove('active');
+
+            }
+            console.log(page.getAttribute('class') + " " + onScreen)
+        })
+
+        // console.log(onScreen);
+    })
 
 }
 
-
-
+function test() {
+    const bd = document.querySelectorAll('body > *');
+    // console.log(bd);
+}
 // function serviceHover() {
 //     const serviceItem = document.querySelector('.service-item');
 //     if (serviceItem) {
@@ -132,10 +158,8 @@ function invokeWhenScroll() {
 // prev.addEventListener('click', () => {
 //     slide.scrollLeft -= 200;
 // })
-
-
-prog();
-// precentsageAnimationBar();
 typeAnimation();
+
+invokeWhenScroll();
 menuHandler();
 serviceHovers();
